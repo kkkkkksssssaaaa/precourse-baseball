e465fbb69bf701ee60cb38faf3f09d20fe34dcaf
kkkkkksssssaaaa <kofc312@naver.com>
2022-03-04 00:05:31 +0900

    test(NumberTest): add internal class for pass or failed case

--- src/test/java/baseball/model/number/NumberTest.java
+++ src/test/java/baseball/model/number/NumberTest.java
@@ -1,29 +1,61 @@
 package baseball.model.number;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberTest {
 
-    @Test
-    void 범위에_포함되지_않는_숫자를_입력하면_IllegalArgumentException을_반환한다() {
-        assertThrows(IllegalArgumentException.class, () -> Number.set(-1));
+    @Nested
+    @DisplayName("Failed Case")
+    class FailedCase {
+
+        @Test
+        void 범위에_포함되지_않는_숫자를_입력하면_IllegalArgumentException을_반환한다() {
+            assertThrows(IllegalArgumentException.class, () -> Number.of(-1));
+        }
+
+        @Test
+        void 같은_값을_가지고_있는_객체를_비교하여_예외를_던질_수_있다() {
+            Number compareLeft = Number.of(5);
+            Number compareRight = Number.of(5);
+
+            assertThrows(IllegalArgumentException.class,
+                    () -> compareLeft.ifEqualsThrowDuplicateException(compareRight));
+        }
+
     }
 
-    @Test
-    void 최소값인_숫자를_입력하면_예외가_반환되지_않는다() {
-        assertDoesNotThrow(() -> Number.set(1));
-    }
+    @Nested
+    @DisplayName("Pass Case")
+    class PassCase {
 
-    @Test
-    void 최대값인_숫자를_입력하면_예외가_반환되지_않는다() {
-        assertDoesNotThrow(() -> Number.set(9));
-    }
+        @Test
+        void 최소값인_숫자를_입력하면_예외가_반환되지_않는다() {
+            assertDoesNotThrow(() -> Number.of(1));
+        }
 
-    @Test
-    void 범위내_숫자를_입력하면_예외가_반환되지_않는다() {
-        assertDoesNotThrow(() -> Number.set(5));
+        @Test
+        void 최대값인_숫자를_입력하면_예외가_반환되지_않는다() {
+            assertDoesNotThrow(() -> Number.of(9));
+        }
+
+        @Test
+        void 범위내_숫자를_입력하면_예외가_반환되지_않는다() {
+            assertDoesNotThrow(() -> Number.of(5));
+        }
+
+        @Test
+        void 같은_값을_가지면_equalsTrue_를_반환한다() {
+            Number compareLeft = Number.of(5);
+            Number compareRight = Number.of(5);
+
+            assertTrue(
+                    compareLeft.equals(compareRight));
+        }
+
     }
 
 }
