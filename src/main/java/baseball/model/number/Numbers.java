6a14f3505bd2e59211d3d3572d0e29648e56475c
kkkkkksssssaaaa <kofc312@naver.com>
2022-03-22 23:35:16 +0900

    feat: add Numbers model

--- src/main/java/baseball/model/number/Numbers.java
+++ src/main/java/baseball/model/number/Numbers.java
@@ -1,7 +1,36 @@
 package baseball.model.number;
 
+import baseball.model.number.exception.NumbersException;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Numbers {
 
-    protected static final Integer MAX_SIZE = 3;
+    private static final int SIZE = 3;
+    private final Set<Number> numbers;
+
+    private Numbers(List<Number> numbers) {
+        this.numbers = toSet(numbers);
+    }
+
+    public static Numbers of(List<Number> numbers) {
+        return new Numbers(numbers);
+    }
+
+    public int size() {
+        return this.numbers.size();
+    }
+
+    private Set<Number> toSet(List<Number> numbers) {
+        Set<Number> numberSet = new HashSet<>(numbers);
+
+        if (numberSet.size() != SIZE) {
+            throw NumbersException.wrongCollectionSize(SIZE);
+        }
+
+        return numberSet;
+    }
 
 }
